23429eb4dcdcdfa4317eee6bae8383cbe9a8900d
maxxrdrgz <maxxrdrgz@gmail.com>
2014-04-16 15:13:53 -0700

    update

--- Lab 5/Identifier.cpp
+++ Lab 5/Identifier.cpp
@@ -9,3 +9,19 @@ Identifier:: ~Identifier()
 {
 
 }
+void Identifier::setLeftChild(Identifier *tok)
+{
+    this->leftChild = tok;
+}
+Identifier* Identifier::getLeftChild()
+{
+    return this->leftChild;
+}
+void Identifier::setRightChild(Identifier *tok)
+{
+    this->rightChild = tok;
+}
+Identifier* Identifier::getRightChild()
+{
+    return this->rightChild;
+}
--- Lab 5/Identifier.h
+++ Lab 5/Identifier.h
@@ -4,13 +4,16 @@
 class Identifier : public Token
 {
   private:
-  
+    Identifier *leftChild;
+    Identifier *rightChild;
   
   public:
     Identifier(){}
     ~Identifier(){}
-    
-    
+    Identifier* getRightChild();
+    Identifier* getLeftChild();
+    void setLeftChild(Identifier *tok);
+    void setRightChild(Identifier *tok);
     
     
 };
